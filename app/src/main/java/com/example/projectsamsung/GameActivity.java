de7852269e95454985ce3cbb02cd4a215c95a979
Erema333 <hackechpro@gmail.com>
2023-03-02 22:15:37 +0700

    sdelat robotosposobnoi

--- app/src/main/java/com/example/projectsamsung/GameActivity.java
+++ app/src/main/java/com/example/projectsamsung/GameActivity.java
@@ -5,13 +5,14 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.projectsamsung.databinding.GameViewBinding;
 import com.example.projectsamsung.databinding.PreViewBinding;
 
 public class GameActivity extends AppCompatActivity {
-    private PreViewBinding binding;
+    private GameViewBinding binding;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        binding = PreViewBinding.inflate(getLayoutInflater());
+        binding = GameViewBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
     }
